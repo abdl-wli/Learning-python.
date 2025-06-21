405343f18481aefddc8dfda5702049d4ccc2b25d
abdl-wli <wli.afzal@gmail.com>
2025-06-21 22:32:04 +0500

    f string added

--- basic arithmetic.py
+++ basic arithmetic.py
@@ -6,3 +6,8 @@ def greet(name):
 greet('Abdul wali')
 greet('Naseeb khan')
 greet('Ahmed')
+
+def greet2(name,f_string):
+    print(f'Hello {name}, {f_string}')
+
+greet2('Abdul wali','im writing with f_string')
