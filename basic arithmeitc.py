76743c8edff825f346d44145e938abefe574941f
abdl-wli <wli.afzal@gmail.com>
2025-06-21 22:14:25 +0500

    very first at git

---
+++ basic arithmeitc.py
@@ -0,0 +1,12 @@
+print('Welcome to python for everybody')
+
+
+def greet(name):
+    print('hello ' + name)
+
+greet("Abdul wali")
+
+def sentence(intro,course):
+    print(f"hello my name is {intro} & i'm brand new to {course}")
+
+sentence('Abdul Wali','python for everybody')
